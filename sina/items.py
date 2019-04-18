e1b966b01535048edd5159bc5a3f8266d5b0df7d
Simon <425854220@qq.com>
2019-04-18 11:23:02 +0800

    Update items.py

--- sina/items.py
+++ sina/items.py
@@ -41,15 +41,3 @@ class RelationshipsItem(Item):
     fan_id = Field()  # 关注者,即粉丝的id
     followed_id = Field()  # 被关注者的id
     crawl_time = Field()  # 抓取时间戳
-
-
-class CommentItem(Item):
-    """
-    微博评论信息
-    """
-    _id = Field()
-    comment_user_id = Field()  # 评论用户的id
-    content = Field()  # 评论的内容
-    weibo_url = Field()  # 评论的微博的url
-    created_at = Field()  # 评论发表时间
-    crawl_time = Field()  # 抓取时间戳
